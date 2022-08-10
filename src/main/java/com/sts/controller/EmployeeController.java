d17949c8daa5ccf999714e479e79ebf56689b9c3
DrUttam <105095298+DrUttam@users.noreply.github.com>
2022-08-10 21:03:30 +0530

    new update

--- src/main/java/com/sts/controller/EmployeeController.java
+++ src/main/java/com/sts/controller/EmployeeController.java
@@ -32,11 +32,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	// get all student
-//	@GetMapping("/students")
-//	public List<EmployeeDetails> getAllStudents(){
-//		return studentRepository.findAll();
-//	}		
+			
 	
 	// create employee rest api
 	@PostMapping("/employees")
@@ -44,59 +40,6 @@ public class EmployeeController {
 		return employeeRepository.save(employee);
 	}
 	
-	// get student by id rest api
-//	@GetMapping("/students/{id}")
-//	public ResponseEntity<EmployeeDetails> getStudentById(@PathVariable Long id) {
-//		EmployeeDetails student = studentRepository.findById(id)
-//				.orElseThrow(() -> new com.sts.exception.ResourceNotFoundException("Student not exist with id :" + id));
-//		return ResponseEntity.ok(student);
-//	}
-//	
-	// update student rest api
 	
-//	@PutMapping("/students/{id}")
-//	public ResponseEntity<EmployeeDetails> updateStudent(@PathVariable Long id, @RequestBody EmployeeDetails studentDetails){
-//		EmployeeDetails student = studentRepository.findById(id)
-//				.orElseThrow(() -> new ResourceNotFoundException("Student not exist with id :" + id));
-//		
-//		student.setfirstName(studentDetails.getfirstName());
-//		student.setaddress(studentDetails.getaddress());
-//		student.setEmailId(studentDetails.getEmailId());
-//		student.setMobileNo(studentDetails.getMobileNo());
-//		student.setParance_mobileNo(studentDetails.getParance_mobileNo());
-//		student.setRegdNo(studentDetails.getRegdNo());
-//		student.setPickUpAddress(studentDetails.getPickUpAddress());
-//	
-//		
-//		EmployeeDetails updatedStudent = studentRepository.save(student);
-//		return ResponseEntity.ok(updatedStudent);
-//	}
-//	
-	// delete student rest api
-//	@DeleteMapping("/students/{id}")
-//	public ResponseEntity<Map<String, Boolean>> deleteStudent(@PathVariable Long id){
-//		EmployeeDetails student = studentRepository.findById(id)
-//				.orElseThrow(() -> new ResourceNotFoundException("Student not exist with id :" + id));
-//		
-//		studentRepository.delete(student);
-//		Map<String, Boolean> response = new HashMap<>();
-//		response.put("deleted", Boolean.TRUE);
-//		return ResponseEntity.ok(response);
-//	}
-	
-	// get student by id rest api
-//	@GetMapping("/students/{name}")
-//	public ResponseEntity<List<Student>> getStudentByName(@PathVariable String name) {
-//		List<Student> listStudents = studentRepository.findByName(name);
-//		if(listStudents.size()==0)
-//				throw new com.sts.exception.ResourceNotFoundException("Student not exist with  :");
-//		return ResponseEntity.ok(listStudents);
-//	}
-
-	// Find By User Name
-//	@GetMapping("/studentsCallByName/{name}")
-//	public List<EmployeeDetails> getAllStudentsByName(@PathVariable String name){
-//		return studentRepository.findByName(name);
-//	}	
 	
 }
